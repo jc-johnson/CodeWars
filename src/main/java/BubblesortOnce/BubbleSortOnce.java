1f0f07fd0dddad716ce1f43541e5c861c3951ee6
Jordan C. Johnson <thePugalist@users.noreply.github.com>
2017-06-04 22:40:19 -0700

    solution

--- src/main/java/BubblesortOnce/BubbleSortOnce.java
+++ src/main/java/BubblesortOnce/BubbleSortOnce.java
@@ -1,7 +1,34 @@
 package src.main.java.BubblesortOnce;
 
+import java.util.Arrays;
+
 /**
  * Created by Jordan on 6/4/2017.
  */
 public class BubbleSortOnce {
+
+    public static Integer[] sort(Integer[] integerArray) {
+
+        for (int i = 0; i < integerArray.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (integerArray[i] < integerArray[j]) {
+                    swap(integerArray, i, j);
+                }
+            }
+        }
+        return integerArray;
+    }
+
+    public static void swap(Integer[] integerArray, Integer index1, Integer index2) {
+        Integer temp = integerArray[index1];
+        integerArray[index1] = integerArray[index2];
+        integerArray[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] test = {9,7,5,3,1,2,4,6,8};
+        test = sort(test);
+        System.out.println(Arrays.toString(test));
+
+    }
 }
