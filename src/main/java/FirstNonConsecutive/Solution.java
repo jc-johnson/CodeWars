527266b3a694ab5af94ed6ada5b24e246a8e22d8
Jordan C. Johnson <thePugalist@users.noreply.github.com>
2017-06-06 11:07:12 -0700

    First non consecutive solution

---
+++ src/main/java/FirstNonConsecutive/Solution.java
@@ -0,0 +1,31 @@
+package src.main.java.FirstNonConsecutive;
+
+/**
+ * Created by Jordan on 6/6/2017.
+ */
+public class Solution {
+
+    static Integer find(final int[] array) {
+
+        if (array == null || array.length == 0) return null;
+
+        for (int i = 0; i < array.length-1; i++) {
+            if (array[i+1] != array[i] + 1) {
+                return array[i+1];
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] array = {0,1,2,6};
+        int[] array1 = {};
+        int[] array2 = {1,2,3};
+        System.out.println(find(array));
+        System.out.println(find(array1));
+        System.out.println(find(array2));
+    }
+}
