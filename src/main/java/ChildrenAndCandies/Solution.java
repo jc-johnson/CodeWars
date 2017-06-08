fd09e0a98dc82c19611d9ff50055e806bda40ccb
jc-johnson <jc-johnson@users.noreply.github.com>
2017-06-08 00:56:53 -0700

    setup

---
+++ src/main/java/ChildrenAndCandies/Solution.java
@@ -0,0 +1,12 @@
+package src.main.java.ChildrenAndCandies;
+
+/**
+ * Created by Jordan on 6/8/2017.
+ */
+public class Solution {
+
+    public static int[] distributionOfCandy(int[] candies) {
+        //coding and coding..
+        return new int[2];
+    }
+}
