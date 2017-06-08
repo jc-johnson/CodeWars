b9c352e8e2ed75b17b8a6685420bdbce25061996
jc-johnson <jc-johnson@users.noreply.github.com>
2017-06-08 01:33:01 -0700

    close solution

--- src/main/java/ChildrenAndCandies/Solution.java
+++ src/main/java/ChildrenAndCandies/Solution.java
@@ -1,12 +1,57 @@
 package src.main.java.ChildrenAndCandies;
 
+import java.util.Arrays;
+
 /**
  * Created by Jordan on 6/8/2017.
  */
 public class Solution {
 
-    public static int[] distributionOfCandy(int[] candies) {
+    public static int[] distributionOfCandy(int[] array) {
+
         //coding and coding..
-        return new int[2];
+        int[] candies = array;
+
+        // TODO: make distribution simultaneous
+        while (!Solution.distributionComplete(candies)) {
+            for (int i = 0; i < candies.length; i++) {
+                System.out.println(candies[i] + " ");
+                while (!(candies[i] % 2 == 0)) {
+                    candies[i] += 1;
+                }
+                int half = candies[i]/2;
+
+                System.out.println("half: " + half);
+
+                // give half of candies to next kid to right
+                if (i < candies.length-1)
+                    candies[i+1] += half;
+                else
+                    array[0] += half;
+            }
+            System.out.println(Arrays.toString(candies));
+        }
+
+        return candies;
+    }
+
+    public static boolean distributionComplete(int[] array) {
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i-1] != array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] array2 = {1, 1, 1, 1, 1,};
+
+        Solution.distributionOfCandy(array);
+
+        // System.out.println(Solution.distributionComplete(array));
+        // System.out.println(Solution.distributionComplete(array2));
     }
 }
