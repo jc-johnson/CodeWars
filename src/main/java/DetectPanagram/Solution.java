db3b6c29d29f4df7fb753a1977d69baa1e20435f
jc-johnson <jc-johnson@users.noreply.github.com>
2017-09-10 18:41:35 -0700

    initial setup

---
+++ src/main/java/DetectPanagram/Solution.java
@@ -0,0 +1,19 @@
+package src.main.java.DetectPanagram;
+
+/**
+ * Created by Jordan on 9/10/2017.
+ */
+public class Solution {
+
+    public boolean check(String sentence) {
+
+        if (sentence == "") return false;
+
+        String localSentence = sentence.toLowerCase();
+
+
+
+        return false;
+
+    }
+}
