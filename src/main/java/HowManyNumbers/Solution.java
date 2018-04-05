f5edef8bc48b015d9c679dfac69b1df1dd711961
jc-johnson <jc-johnson@users.noreply.github.com>
2018-04-05 13:33:27 -0700

    setup

---
+++ src/main/java/HowManyNumbers/Solution.java
@@ -0,0 +1,20 @@
+package src.main.java.HowManyNumbers;
+
+public class Solution {
+
+
+    public static int sel_number(int upperLimit, int maxDifference) {
+
+        buildDigit();
+
+        return 0;
+    }
+
+    private static int buildDigit(){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
