45dc913eab63ce0f45806f274a32e3abffd5dc1d
jc-johnson <jc-johnson@users.noreply.github.com>
2018-04-06 09:19:32 -0700

    solution

--- src/main/java/HowManyNumbers/Solution.java
+++ src/main/java/HowManyNumbers/Solution.java
@@ -1,5 +1,8 @@
 package src.main.java.HowManyNumbers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
 
@@ -8,16 +11,20 @@ public class Solution {
         int digit = 0;
         int n = 1;
         int j = n + 1;
+        List<Integer> numberList = new ArrayList<>();
 
         while(digit < upperLimit){
 
             StringBuilder stringBuilder = new StringBuilder();
-            if(j - n <= maxDifference) {
+            if( j - n <= maxDifference) {
                 stringBuilder.append(n);
                 stringBuilder.append(j);
                 String intString = stringBuilder.toString();
                 digit = Integer.parseInt(intString);
                 System.out.println(digit);
+                if (digit < upperLimit) {
+                    numberList.add(digit);
+                }
             }
 
             j++;
@@ -27,10 +34,11 @@ public class Solution {
                 j = n + 1;
             }
         }
-        return 0;
+        System.out.println("Number list size: " + numberList.size());
+        return numberList.size();
     }
 
     public static void main(String[] args) {
-        sel_number(30, 2);
+        sel_number(50, 5);
     }
 }
